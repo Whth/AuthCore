19942d5f7877c1ab798a3cfbb81e3662a38938f3
Whth <88489697+Whth@users.noreply.github.com>
2023-10-09 21:57:34 +0800

    feat(extension): authCore init
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ from modules.plugin_base import AbstractPlugin
 __all__ = ["AuthCore"]
 
 
-class AuthCore(AbstractPlugin):
-    CONFIG_DETECTED_KEYWORD = "detected_keyword"
+class CMD:
+    ROOT = "auth"
+    LIST = "list"
+    PERM = "perm"
+    USER = "user"
+    ROLE = "role"
+    SRC = "src"
+    GRANT = "grant"
+    New = "new"
+    Delete = "del"
 
+
+class Mode:
+    Perm2Role = "pr"
+    Role2User = "ru"
+
+
+class AuthCore(AbstractPlugin):
     def _get_config_parent_dir(self) -> str:
         return os.path.abspath(os.path.dirname(__file__))
 
@@ -28,21 +43,221 @@ class AuthCore(AbstractPlugin):
         return "Whth"
 
     def __register_all_config(self):
-        self._config_registry.register_config(self.CONFIG_DETECTED_KEYWORD, "auth")
+        pass
 
     def install(self):
-        from graia.ariadne.message.parser.base import ContainKeyword
-        from graia.ariadne.model import Group
-        from graia.ariadne.event.message import GroupMessage
+        from modules.cmd import RequiredPermission, NameSpaceNode, ExecutableNode
+        from modules.auth.resources import required_perm_generator
+        from modules.auth.permissions import Permission, PermissionCode
 
         self.__register_all_config()
         self._config_registry.load_config()
 
-        from graia.ariadne import Ariadne
+        def grant_perm_to_role(perm_label: str, role_label: str) -> str:
+            """
+            Grant permission to a role.
 
-        @self.receiver(
-            GroupMessage,
-            decorators=[ContainKeyword(keyword=self._config_registry.get_config(self.CONFIG_DETECTED_KEYWORD))],
+            Args:
+                perm_label (str): The label of the permission to grant.
+                role_label (str): The label of the role to grant the permission to.
+
+            Returns:
+                str: A string indicating the success of the permission grant operation.
+            """
+            stdout = f"Grant {perm_label} to {role_label}\nSuccess = {self._auth_manager.grant_perm_to_role(perm_label, role_label)}"
+            return stdout
+
+        def grant_role_to_user(role_label: str, user_label: str) -> str:
+            """
+            Grant a role to a user.
+
+            Args:
+                role_label (str): The label of the role to grant.
+                user_label (str): The label of the user to grant the role to.
+
+            Returns:
+                str: A string indicating the success of the operation.
+
+            """
+            stdout = f"Grant {role_label} to {user_label}\nSuccess = {self._auth_manager.grant_role_to_user(role_label, user_label)}"
+            return stdout
+
+        def new_permission(perm_id: int, perm_name: str) -> str:
+            """
+            Creates a new permission with the given permission ID and permission name.
+
+            Parameters:
+            - perm_id (int): The ID of the new permission.
+            - perm_name (str): The name of the new permission.
+
+            Returns:
+            - str: A string indicating the success of creating the new permission. It includes the permission ID and name, as well as the result of adding the permission to the authentication manager.
+            """
+            stdout = f"New permission {perm_id} {perm_name}\nSuccess = {self._auth_manager.add_perm_from_info(perm_id, perm_name)}"
+            return stdout
+
+        def delete_permission(perm_id: int, perm_name: str) -> str:
+            """
+            Delete a permission with the given ID and name.
+
+            :param perm_id: The ID of the permission to delete.
+            :param perm_name: The name of the permission to delete.
+            :return: A string indicating the success or failure of the deletion.
+            """
+            stdout = f"Delete permission {perm_id}\nSuccess = {self._auth_manager.remove_perm(perm_id, perm_name)}"
+            return stdout
+
+        def new_role(role_id: int, role_name: str) -> str:
+            """
+            Creates a new role with the given role ID and role name.
+
+            Parameters:
+                role_id (int): The ID of the new role.
+                role_name (str): The name of the new role.
+
+            Returns:
+                str: A string indicating the success of the operation. It includes the new role ID and role name, as well as whether the role was successfully added.
+            """
+            stdout = f"New role {role_id} {role_name}\nSuccess = {self._auth_manager.add_role(role_id, role_name)}"
+            return stdout
+
+        def delete_role(role_id: int, role_name: str) -> str:
+            """
+            Delete a role.
+
+            Args:
+                role_id (int): The ID of the role to delete.
+                role_name (str): The name of the role to delete.
+
+            Returns:
+                str: A string indicating the result of the deletion.
+            """
+            stdout = f"Delete role {role_id}\nSuccess = {self._auth_manager.remove_role(role_id, role_name)}"
+            return stdout
+
+        def new_user(user_id: int, user_name: str) -> str:
+            """
+            Adds a new user to the system.
+
+            Args:
+                user_id (int): The unique identifier for the user.
+                user_name (str): The name of the user.
+
+            Returns:
+                str: A string indicating the success of adding the user.
+            """
+            stdout = f"New user {user_id} {user_name}\nSuccess = {self._auth_manager.add_user(user_id, user_name)}"
+            return stdout
+
+        def delete_user(user_id: int, user_name: str) -> str:
+            """
+            Deletes a user with the given user ID and user name.
+
+            Args:
+                user_id (int): The ID of the user to be deleted.
+                user_name (str): The name of the user to be deleted.
+
+            Returns:
+                str: A string indicating the success of the deletion operation.
+            """
+            stdout = f"Delete user {user_id}\nSuccess = {self._auth_manager.remove_user(user_id, user_name)}"
+            return stdout
+
+        su_perm = Permission(id=PermissionCode.SuperPermission.value, name=self.get_plugin_name())
+        req_perm: RequiredPermission = required_perm_generator(
+            target_resource_name=self.get_plugin_name(), super_permissions=[su_perm]
         )
-        async def hello(app: Ariadne, group: Group):
-            await app.send_message(group, "hello")
+        tree = NameSpaceNode(
+            name=CMD.ROOT,
+            required_permissions=req_perm,
+            help_message=self.get_plugin_description(),
+            children_node=[
+                NameSpaceNode(
+                    name=CMD.LIST,
+                    required_permissions=req_perm,
+                    help_message="allow view of authorization elements",
+                    children_node=[
+                        ExecutableNode(
+                            name=CMD.PERM,
+                            source=lambda: "\n".join([perm.unique_label for perm in self._auth_manager.permissions]),
+                        ),
+                        ExecutableNode(
+                            name=CMD.USER,
+                            source=lambda: "\n".join([user.unique_label for user in self._auth_manager.users]),
+                        ),
+                        ExecutableNode(
+                            name=CMD.ROLE,
+                            source=lambda: "\n".join([role.unique_label for role in self._auth_manager.roles]),
+                        ),
+                        ExecutableNode(
+                            name=CMD.SRC,
+                            source=lambda: "\n".join([src.unique_label for src in self._auth_manager.resources]),
+                        ),
+                    ],
+                ),
+                NameSpaceNode(
+                    name=CMD.GRANT,
+                    required_permissions=req_perm,
+                    help_message="allow to grant permissions\nAdd Perm to Role\nAdd Role to User",
+                    children_node=[
+                        ExecutableNode(
+                            name=Mode.Perm2Role,
+                            source=grant_perm_to_role,
+                            help_message=grant_perm_to_role.__doc__,
+                        ),
+                        ExecutableNode(
+                            name=Mode.Role2User,
+                            source=grant_role_to_user,
+                            help_message=grant_role_to_user.__doc__,
+                        ),
+                    ],
+                ),
+                NameSpaceNode(
+                    name=CMD.New,
+                    required_permissions=req_perm,
+                    help_message="allow to create new perm,role,user",
+                    children_node=[
+                        ExecutableNode(
+                            name=CMD.PERM,
+                            source=new_permission,
+                            help_message=new_permission.__doc__,
+                        ),
+                        ExecutableNode(
+                            name=CMD.ROLE,
+                            source=new_role,
+                            help_message=new_role.__doc__,
+                        ),
+                        ExecutableNode(
+                            name=CMD.USER,
+                            source=new_user,
+                            help_message=new_user.__doc__,
+                        ),
+                    ],
+                ),
+                NameSpaceNode(
+                    name=CMD.Delete,
+                    required_permissions=req_perm,
+                    help_message="allow to delete perm,role,user",
+                    children_node=[
+                        ExecutableNode(
+                            name=CMD.PERM,
+                            source=delete_permission,
+                            help_message=delete_permission.__doc__,
+                        ),
+                        ExecutableNode(
+                            name=CMD.ROLE,
+                            source=delete_role,
+                            help_message=delete_role.__doc__,
+                        ),
+                        ExecutableNode(
+                            name=CMD.USER,
+                            source=delete_user,
+                            help_message=delete_user.__doc__,
+                        ),
+                    ],
+                ),
+            ],
+        )
+
+        self._auth_manager.add_perm_from_req(req_perm)
+        self._root_namespace_node.add_node(tree)
